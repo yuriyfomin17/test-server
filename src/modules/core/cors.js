bab45a3670b431991be23452d118bf9a6cff2be8
yuriyfomin17 <fominyura2012@gmail.com>
2021-01-14 22:21:49 +0200

    Updated

--- src/modules/core/cors.js
+++ src/modules/core/cors.js
@@ -2,7 +2,6 @@ export default function cors(app) {
   app.use((req, res, next) => {
     // eslint-disable-line consistent-return
     res.header('Access-Control-Allow-Origin', '*');
-    req.header('Access-Control-Allow-Origin', '*')
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
